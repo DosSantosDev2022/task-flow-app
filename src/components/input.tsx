781d58019976fde804b9e8ee6567f4ab207575da
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-06-04 16:39:27 -0300

    feat : Criando componente Input

---
+++ src/components/input.tsx
@@ -0,0 +1,48 @@
+import React, { ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export interface InputRootProps {
+  children: ReactNode
+}
+
+export interface InputIconProps {
+  children: ReactNode
+}
+
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
+
+function InputRoot({ children }: InputRootProps) {
+  return (
+    <div className="flex h-10 w-full items-center gap-3 rounded-2xl  bg-zinc-200 px-3 py-4 focus-within:ring-2 focus-within:ring-zinc-400">
+      {children}
+    </div>
+  )
+}
+
+function InputIcon({ children }: InputIconProps) {
+  return <i>{children}</i>
+}
+
+const ComponentInput = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={twMerge(
+          'text-zinc-600 text-md flex-1 bg-transparent font-light outline-none placeholder:text-zinc-400',
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  },
+)
+
+ComponentInput.displayName = 'Input'
+
+export const Input = {
+  Root: InputRoot,
+  Icon: InputIcon,
+  Input: ComponentInput,
+}
