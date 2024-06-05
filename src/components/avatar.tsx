69e0134b08fe3650af9a805fd7f7a3e21e40c89a
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-06-05 17:16:08 -0300

    feat: criando componente avatar

---
+++ src/components/avatar.tsx
@@ -0,0 +1,39 @@
+'use client'
+import Image from 'next/image'
+import { useState } from 'react'
+
+interface AvatarProps {
+  Url: string
+  Alt: string
+  name: string
+}
+
+export function Avatar({ Alt, Url, name }: AvatarProps) {
+  const [imageError, setImageError] = useState(false)
+
+  const handleError = () => {
+    setImageError(true)
+  }
+  const getInitial = (name: string) => {
+    return name.charAt(0).toUpperCase()
+  }
+  return (
+    <div className="relative inline-flex h-10 w-10 items-center justify-center">
+      {imageError ? (
+        <div className="flex h-full w-full items-center justify-center rounded-full bg-gray-300 text-xl text-zinc-800">
+          {getInitial(name)}
+        </div>
+      ) : (
+        <Image
+          className="h-10 w-10 rounded-full border-2 border-white"
+          alt={Alt}
+          src={Url}
+          width={40}
+          height={40}
+          quality={100}
+          onError={handleError}
+        />
+      )}
+    </div>
+  )
+}
