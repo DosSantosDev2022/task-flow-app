ec28072d0664fb7e689cffc140edcd7b058effba
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-06-05 17:16:25 -0300

    styles : alterações no componente button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -22,16 +22,16 @@ export function Button({
     primary: `bg-zinc-900 text-zinc-50 hover:bg-zinc-800  `,
     secundary: 'bg-zinc-50 text-zinc-900 hover:bg-zinc-100',
     outline: `bg-transparent border border-zinc-800 text-zinc-50 hover:bg-zinc-600`,
-    highlight: `text-zinc-50 hover:bg-violet-800 duration-300 bg-violet-900 `,
+    highlight: `text-zinc-50 hover:bg-green-800 duration-300 bg-green-600 `,
     disabled: 'bg-zinc-800 border border-zinc-800 text-cyan-50 ',
-    link: `bg-transparent border-none underline`,
+    link: `bg-transparent border-none`,
     danger: `bg-red-700 hover:bg-red-600 text-zinc-50`,
     warning: `bg-yellow-400 text-zinc-900 hover:bg-yellow-300`,
   }
 
   const _className = twMerge(
     variantClasses[variant],
-    `appearance-none rounded-md px-2 py-2 text-sm font-bold shadow transition-all duration-500 w-[96px] `,
+    `appearance-none rounded-md px-2 py-2 text-sm font-bold  transition-all duration-500 w-[96px] `,
     className,
   )
 
