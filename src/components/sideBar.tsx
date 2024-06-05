f36b609ac4bdcd59e1d28f06c0d5a2df72c4aad8
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-06-05 17:17:50 -0300

    feat: adicionando estado aberto e fechado ao SideBar

--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -1,10 +1,19 @@
-import { MdEditDocument, MdSpaceDashboard } from 'react-icons/md'
+'use client'
+
+import {
+  MdClose,
+  MdEditDocument,
+  MdMenu,
+  MdSpaceDashboard,
+} from 'react-icons/md'
 import { Logo } from './logo'
 import { Navigation } from './navigation'
 import { NavigationLinks } from './navigation/navigationLink'
 import { FaUsers } from 'react-icons/fa'
 import { SiGoogletasks } from 'react-icons/si'
 import { FaGear } from 'react-icons/fa6'
+import { Button } from './button'
+import { useState } from 'react'
 
 export function SideBar() {
   const navigationLinks = [
@@ -35,10 +44,21 @@ export function SideBar() {
     },
   ]
 
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <aside className="w-[257px] h-full bg-zinc-50 px-4 py-6 space-y-4">
-      <div className="flex items-center w-full p-2 border-b border-zinc-200 ">
+    <aside
+      className={`${isOpen ? 'w-[257px] ' : 'w-20'} h-full flex duration-500 shadow-lg border  flex-col items-start justify-start bg-zinc-50 px-4 py-6 space-y-4`}
+    >
+      <div className="flex items-center justify-between w-full p-2 gap-2 border-b border-zinc-200 ">
         <Logo />
+        <Button
+          className=" w-[40] h-[40] bg-zinc-50 rounded-full shadow-lg flex items-center justify-center"
+          onClick={() => setIsOpen(!isOpen)}
+          variant="link"
+        >
+          {isOpen ? <MdClose size={25} /> : <MdMenu size={25} />}
+        </Button>
       </div>
       <div className="p-2 h-full flex flex-col justify-start">
         <Navigation.Root>
@@ -47,7 +67,7 @@ export function SideBar() {
               <Navigation.Item key={link.label}>
                 <NavigationLinks url={link.Url}>
                   <Navigation.Icon>{link.icon} </Navigation.Icon>
-                  {link.label}
+                  {isOpen && <span>{link.label}</span>}
                 </NavigationLinks>
               </Navigation.Item>
             ))}
