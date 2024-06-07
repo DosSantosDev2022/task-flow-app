49d34c5ba20245c81cf81cfbebf9f1e99330dfcd
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-06-07 15:59:03 -0300

    feat: criando componente label

---
+++ src/components/global/label.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export type LabelProps = React.LabelHTMLAttributes<HTMLLabelElement>
+
+export function Label({ ...props }: LabelProps) {
+  return (
+    <label
+      {...props}
+      className="text-base font-normal leading-[25.6px] text-zinc-600
+    "
+    />
+  )
+}
