e332e32365cfb612925ce51a3be3e596c375ea41
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-06-07 16:02:16 -0300

    feat: adicionando novas funcionalidades ao componente filterTasks

--- src/components/pages/tasks/filterTasks.tsx
+++ src/components/pages/tasks/filterTasks.tsx
@@ -1,44 +1,53 @@
-import { LuPlus } from 'react-icons/lu'
-import { Button } from './button'
-import Link from 'next/link'
+'use client'
+import { AddNewTasksModal } from './addTasks'
+import { useState } from 'react'
+import { Button } from '@/components/global/button'
+import { FilterType } from './tasks'
 
-export function FilterTasks() {
-  const links = [
+interface FilterTasksProps {
+  onFilterChange: (filter: FilterType) => void
+}
+
+export function FilterTasks({ onFilterChange }: FilterTasksProps) {
+  const status: { label: string; value: FilterType }[] = [
     {
       label: 'Todas',
-      url: '',
+      value: 'all',
     },
     {
       label: 'A fazer',
-      url: '',
+      value: 'todo',
     },
     {
       label: 'Em andamento',
-      url: '',
+      value: 'in-progress',
     },
     {
       label: 'Concluído',
-      url: '',
+      value: 'completed',
     },
   ]
+
+  const [activeFilter, setActiveFilter] = useState('all')
+  const handleFilterClick = (filter: FilterType) => {
+    setActiveFilter(filter)
+    onFilterChange(filter)
+  }
   return (
     <div className="flex items-center gap-2 px-2 py-4">
-      <Button
-        variant="highlight"
-        className="flex items-center gap-2 justify-center rounded-xl h-[46px]"
-      >
-        <LuPlus size={18} />
-        New
-      </Button>
+      <AddNewTasksModal />
       <div className="rounded-xl w-full bg-zinc-50 h-[46px] flex justify-start px-4 py-2 items-center gap-11 ">
-        {links.map((link) => (
-          <Link
-            key={link.label}
-            href={link.url}
-            className="hover:text-green-600 duration-300 px-2 py-3 rounded-lg  text-zinc-600"
+        {status.map((status) => (
+          <Button
+            variant="link"
+            key={status.label}
+            onClick={() => handleFilterClick(status.value)}
+            className={`hover:text-violet-600 text-sm w-full  duration-300 px-2 py-3 rounded-lg text-zinc-600 ${
+              activeFilter === status.value ? 'text-violet-600 bg-zinc-200' : ''
+            }`}
           >
-            {link.label}
-          </Link>
+            {status.label}
+          </Button>
         ))}
       </div>
     </div>
