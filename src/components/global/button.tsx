dff8954a38e22bdbe8bc9044a8494e14f4ce0eb3
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-09-03 07:31:22 -0300

    FEAT: Atualizando componente button

--- src/components/global/button.tsx
+++ src/components/global/button.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import * as React from 'react'
+import { Slot } from '@radix-ui/react-slot'
+import { ForwardedRef } from 'react'
 import { twMerge } from 'tailwind-merge'
+import { FaSpinner } from 'react-icons/fa6'
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  isLoading?: boolean
+  asChild?: boolean
+  sizes?: 'xs' | 'sm' | 'lg' | 'icon' | 'full'
   variant?:
     | 'primary'
     | 'secundary'
@@ -12,33 +18,66 @@ export interface ButtonProps
     | 'link'
     | 'danger'
     | 'warning'
+    | 'Shine'
+    | 'Swipe'
+  ref?: ForwardedRef<HTMLButtonElement>
 }
 
 export function Button({
   className,
+  isLoading = false,
+  sizes = 'xs',
   variant = 'primary',
+  asChild = false,
+  ref,
   ...props
 }: ButtonProps) {
   const variantClasses = {
-    primary: `bg-zinc-900 text-zinc-50 hover:bg-zinc-800  `,
-    secundary: 'bg-zinc-50 text-zinc-900 hover:bg-zinc-100',
-    outline: `bg-transparent border border-zinc-800 text-zinc-50 hover:bg-zinc-600`,
-    highlight: `text-zinc-50 hover:bg-green-800 duration-300 bg-green-600 `,
-    disabled: 'bg-zinc-800 border border-zinc-800 text-cyan-50 ',
-    link: `bg-transparent border-none`,
-    danger: `bg-red-700 hover:bg-red-600 text-zinc-50`,
-    warning: `bg-yellow-400 text-zinc-900 hover:bg-yellow-300`,
+    primary: 'bg-primary-900 text-secondary-50 hover:bg-primary-800',
+    secundary: 'bg-secondary-50 text-primary-900 hover:bg-secondary/50',
+    outline:
+      'bg-transparent border border-secundary text-light hover:bg-primary ',
+    highlight: 'text-zinc-50 hover:bg-violet-800 duration-300 bg-violet-900',
+    disabled:
+      'bg-opacity-80 bg-primary-950 border border-primary-800 text-secondary-950',
+    link: 'bg-transparent border-none underline-offset-4 hover:underline',
+    danger: 'bg-red-700 hover:bg-red-600 text-zinc-50',
+    warning: 'bg-yellow-400 text-zinc-900 hover:bg-yellow-300',
+    Shine:
+      'before:ease relative  overflow-hidden border border-primary-800 bg-primary-800 text-white shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-primary-500 hover:before:-translate-x-40',
+    Swipe:
+      'text-zinc-50 hover:before:bg-redborder-red-500 relative  overflow-hidden border border-primary-500 bg-white text-primary-500 shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-primary-500 before:transition-all before:duration-500 hover:text-white hover:shadow-primary-500 hover:before:left-0 hover:before:w-full',
+  }
+  const sizeClasses = {
+    xs: 'h-10 w-20  px-3 py-2 text-xs',
+    sm: 'h-14 w-24  px-3 py-2 text-sm',
+    lg: 'h-16 w-28  px-8 py-4 text-lg',
+    icon: 'h-9 w-9 p-1 ',
+    full: 'h-10 px-4 py-2 w-full text-lg',
   }
 
   const _className = twMerge(
     variantClasses[variant],
-    `appearance-none rounded-md px-2 py-2 text-sm font-bold  transition-all duration-500 w-[96px] `,
+    sizeClasses[sizes],
+    `appearance-none rounded-md  flex items-center justify-center  font-normal transition-all duration-500 `,
     className,
   )
-
+  const Comp = asChild ? Slot : 'button'
   return (
-    <button className={_className} {...props}>
-      {props.children}
-    </button>
+    <Comp
+      ref={ref}
+      className={_className}
+      {...props}
+      disabled={isLoading || variant === 'disabled'}
+    >
+      {isLoading ? (
+        <span className="flex items-center w-full gap-1">
+          {props.children}
+          <FaSpinner size={20} className="animate-spin text-zinc-50" />
+        </span>
+      ) : (
+        props.children
+      )}
+    </Comp>
   )
 }
