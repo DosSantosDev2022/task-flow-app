828b060d53183901b93182c7bae930568ca4f6d8
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-09-05 12:01:00 -0300

    FEAT: Adicionando tabela de cliente e corrigindo relacionamentos

---
+++ prisma/migrations/20240905145922_update_client_and_project_relations/migration.sql
@@ -0,0 +1,29 @@
+-- AlterTable
+ALTER TABLE "projects" ADD COLUMN     "clientId" TEXT;
+
+-- CreateTable
+CREATE TABLE "clients" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT,
+    "address" TEXT,
+    "city" TEXT,
+    "state" TEXT,
+    "postalCode" TEXT,
+    "country" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "clients_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "clients_email_key" ON "clients"("email");
+
+-- AddForeignKey
+ALTER TABLE "clients" ADD CONSTRAINT "clients_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "projects" ADD CONSTRAINT "projects_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "clients"("id") ON DELETE SET NULL ON UPDATE CASCADE;
