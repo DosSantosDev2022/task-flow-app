8bba2addf67d837aa29e6e86fb99e5bae2d83bd1
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-09-06 22:12:47 -0300

    FEAT: Criando rota para buscar todos os projetos

--- src/app/api/project/route.ts
+++ src/app/api/project/route.ts
@@ -1,18 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import { prisma } from '@/lib/prisma'
 
 export async function GET(req: NextRequest) {
-  if (req.method === 'GET') {
-    try {
-      const projects = await prisma.project.findMany()
-      return NextResponse.json(projects, { status: 200 })
-    } catch (error) {
-      return NextResponse.json(
-        { error: 'Erro ao buscar projetos' },
-        { status: 500 },
-      )
-    }
+  try {
+    const { searchParams } = new URL(req.url)
+    const page = parseInt(searchParams.get('page') || '1')
+    const limit = parseInt(searchParams.get('limit') || '10')
+
+    const projects = await prisma.project.findMany({
+      skip: (page - 1) * limit,
+      take: limit,
+    })
+
+    return NextResponse.json(projects, { status: 200 })
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Erro ao buscar projetos' },
+      { status: 500 },
+    )
   }
 }
