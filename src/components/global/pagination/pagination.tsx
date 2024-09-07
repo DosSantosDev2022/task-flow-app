b88e5a3b515045245461b50599975adb6951b751
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-09-06 22:07:27 -0300

    FEAT: Criando componente pagination

---
+++ src/components/global/pagination/pagination.tsx
@@ -0,0 +1,79 @@
+'use client'
+
+import { usePagination } from '@/hooks/usePagination'
+import { Button } from '@/components/global/button'
+import Link from 'next/link'
+import { LuChevronsLeft, LuChevronsRight } from 'react-icons/lu'
+
+interface PaginationProps {
+  page: number
+  limit: number
+  total: number
+}
+
+export function Pagination({ page, limit, total }: PaginationProps) {
+  const { pages } = usePagination({
+    page,
+    limit,
+    total,
+  })
+
+  const isFirstPage = page === 1
+  const isLastPage = page === Math.ceil(total / limit)
+
+  return (
+    <div className="mt-8 flex w-full items-center justify-between p-2">
+      <span className="flex w-full font-light text-light">
+        Mostrando {Math.min(limit, total - (page - 1) * limit)} de {total}
+      </span>
+
+      <div className="flex items-center gap-2">
+        <Button
+          className={`flex items-center justify-center ${
+            isFirstPage ? 'pointer-events-none opacity-50' : ''
+          }`}
+          sizes="icon"
+          variant="outline"
+          asChild
+        >
+          <Link href={!isFirstPage ? `/Projects?page=1` : '#'} passHref>
+            <LuChevronsLeft />
+          </Link>
+        </Button>
+        {pages.map((pageNumber) => (
+          <Button
+            variant="outline"
+            asChild
+            sizes="icon"
+            key={pageNumber}
+            className={`flex  items-center justify-center ${
+              page === pageNumber ? 'pointer-events-none border opacity-50' : ''
+            }`}
+          >
+            <Link
+              href={page !== pageNumber ? `/Projects?page=${pageNumber}` : '#'}
+            >
+              {pageNumber}
+            </Link>
+          </Button>
+        ))}
+
+        <Button
+          variant="outline"
+          sizes="icon"
+          className={`flex items-center justify-center ${
+            isLastPage ? 'pointer-events-none opacity-50' : ''
+          }`}
+          asChild
+        >
+          <Link
+            href={!isLastPage ? `/Projects?page=${page + 1}` : '#'}
+            passHref
+          >
+            <LuChevronsRight />
+          </Link>
+        </Button>
+      </div>
+    </div>
+  )
+}
