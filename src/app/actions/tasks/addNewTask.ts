2ec28055e21915d59f8695964f5bf325ef68c008
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-09-09 23:07:14 -0300

    FEAT: Criando server action para cadastro de tarefas

---
+++ src/app/actions/tasks/addNewTask.ts
@@ -0,0 +1,29 @@
+'use server'
+import { FormTaskSchema, TaskFormData } from '@/@types/schemas/FormSchemaTasks'
+import { prisma } from '@/lib/prisma'
+import { revalidatePath } from 'next/cache'
+
+export async function addNewTaskAction(dataTask: TaskFormData) {
+  try {
+    console.log('Dados recebidos do form task', dataTask)
+
+    const validatedData = FormTaskSchema.parse(dataTask)
+    console.log(validatedData)
+
+    // Criar a task vinculada a um projectId
+    const newTask = await prisma.task.create({
+      data: {
+        ...validatedData,
+        startDate: new Date(validatedData.startDate),
+        endDate: new Date(validatedData.endDate),
+        projectId: validatedData.projectId
+      },
+    })
+    revalidatePath('/tasks')
+    console.log(newTask)
+    return newTask
+   
+  } catch (error) {
+    console.error('Erro ao adicionar nova tarefa:', error)
+  }
+}
