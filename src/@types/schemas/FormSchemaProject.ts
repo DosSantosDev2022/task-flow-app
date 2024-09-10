3c0ece98810141898c6798f8e047363ca34fec17
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-09-09 23:07:46 -0300

    Feat: Criando types para os formulários de projetos e tarefas

--- src/@types/schemas/FormSchemaProject.ts
+++ src/@types/schemas/FormSchemaProject.ts
@@ -1,11 +1,6 @@
-import { z } from "zod"
+import { z } from 'zod'
 
-const paymentSchema = [
-  'DINHEIRO',
-  'CREDITO',
-  'DEBITO',
-  'PIX',
-] as const
+const paymentSchema = ['DINHEIRO', 'CREDITO', 'DEBITO', 'PIX'] as const
 
 export const FormSchema = z.object({
   title: z.string().min(1, 'O nome do projeto é obrigatório'),
@@ -14,7 +9,7 @@ export const FormSchema = z.object({
   endDate: z.string(),
   price: z.string().min(1, 'O preço é obrigatório'),
   payment: z.enum(paymentSchema),
-  userId: z.string()
+  userId: z.string(),
 })
 
 export type FormDataProject = z.infer<typeof FormSchema>
--- src/@types/schemas/FormSchemaTasks.ts
+++ src/@types/schemas/FormSchemaTasks.ts
@@ -1,11 +1,13 @@
-import { z } from "zod";
+import { TaskStatus } from '@prisma/client'
+import { z } from 'zod'
 
 export const FormTaskSchema = z.object({
-  taskName: z.string().min(1, 'O nome da tarefa é obrigatório'),
-  projectName: z.string().min(1, 'Selecione um projeto'),
+  title: z.string().min(1, 'O nome da tarefa é obrigatório'),
   startDate: z.string().min(1, 'Selecione uma data inicial'),
   endDate: z.string().min(1, 'Selecione uma data de entrega'),
   description: z.string().min(1, 'A descrição da tarefa é obrigatória'),
+  userId: z.string(),
+  projectId: z.string(),
 })
 
 export type TaskFormData = z.infer<typeof FormTaskSchema>
