67cabbcb48af52a9ee3e9e91526f99501a53ac6a
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-09-09 23:10:31 -0300

    FEAT: Criando rota para buscar todos os projetos , com paginação

---
+++ src/app/api/projects/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { prisma } from '@/lib/prisma'
+/* import { getServerSession } from 'next-auth'
+import { authOptions } from '../auth/[...nextauth]/route' */
+
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url)
+    const page = parseInt(searchParams.get('page') || '1')
+    const limit = parseInt(searchParams.get('limit') || '10')
+
+    // Verificar autenticação e obter sessão
+    /*  const session = await getServerSession(authOptions)
+
+    console.log('Sessão obtida:', session)
+    if (!session || !session.user?.id) {
+      return NextResponse.json(
+        { error: 'Usuário não autenticado' },
+        { status: 401 },
+      )
+    }
+ */
+    // Obter ID do usuário da sessão
+    /* const userId = session?.user.id */
+    /* console.log('userId da seção', userId) */
+    const projects = await prisma.project.findMany({
+      /*  where: { userId }, */
+      skip: (page - 1) * limit,
+      take: limit,
+    })
+
+    const totalProjects = await prisma.project.count({
+      /*  where: { userId }, */
+    })
+
+    return NextResponse.json({ projects, totalProjects }, { status: 200 })
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Erro ao buscar projetos' },
+      { status: 500 },
+    )
+  }
+}
