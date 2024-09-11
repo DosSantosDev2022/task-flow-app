b52a7730c5a570f6c7940df4cb437369ddcc6a07
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-09-11 01:40:53 -0300

    FIX: Corrigindo componentes de filtro

--- src/components/pages/projects/filters/FilterByDate.tsx
+++ src/components/pages/projects/filters/FilterByDate.tsx
@@ -1,49 +1,54 @@
 import { Button } from '@/components/global/button'
 import { Input } from '@/components/global/input'
 import { Label } from '@/components/global/label'
-import * as Popover from '@radix-ui/react-popover'
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/global/popover'
 import { IoFilterCircle } from 'react-icons/io5'
 
 export function FilterByDate() {
   return (
     <>
-      <Popover.Root>
-        <Popover.Trigger className="rounded-lg w-28 bg-transparent border px-1.5 py-2 text-zinc-700 gap-2 flex items-center justify-center">
-          <IoFilterCircle />
+      <Popover>
+        <PopoverTrigger>
+          <IoFilterCircle size={24} />
           <span>Data</span>
-        </Popover.Trigger>
+        </PopoverTrigger>
 
-        <Popover.PopoverPortal>
-          <Popover.Content
-            className="w-72 p-4 bg-white rounded-lg shadow-lg border"
-            sideOffset={5}
-          >
-            <h3 className="text-base font-medium mb-2">Filtros</h3>
+        <PopoverContent
+          className="w-72 p-4 bg-white rounded-lg shadow-lg border"
+          sideOffset={5}
+          side="bottom"
+          align="end"
+        >
+          <h3 className="text-base font-medium mb-2">Filtros</h3>
 
-            <div className="flex flex-col gap-2">
-              <div className="flex flex-col gap-1">
-                <Label className="">Data de Início</Label>
-                <Input.Root>
-                  <Input.Input className="text-sm" type="date" />
-                </Input.Root>
-              </div>
-
-              <div className="flex flex-col gap-1">
-                <Label className="">Data de Fim</Label>
-                <Input.Root>
-                  <Input.Input className="text-sm" type="date" />
-                </Input.Root>
-              </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-1">
+              <Label className="">Data de Início</Label>
+              <Input.Root>
+                <Input.Input className="text-sm" type="date" />
+              </Input.Root>
             </div>
 
-            <Popover.Close asChild>
-              <Button sizes="full" variant="highlight" className="mt-4 text-md">
-                Aplicar
-              </Button>
-            </Popover.Close>
-          </Popover.Content>
-        </Popover.PopoverPortal>
-      </Popover.Root>
+            <div className="flex flex-col gap-1">
+              <Label className="">Data de Fim</Label>
+              <Input.Root>
+                <Input.Input className="text-sm" type="date" />
+              </Input.Root>
+            </div>
+          </div>
+
+          <PopoverClose asChild>
+            <Button sizes="full" variant="highlight" className="mt-4 text-md">
+              Aplicar
+            </Button>
+          </PopoverClose>
+        </PopoverContent>
+      </Popover>
     </>
   )
 }
--- src/components/pages/projects/filters/FilterByPriority.tsx
+++ src/components/pages/projects/filters/FilterByPriority.tsx
@@ -1,5 +1,10 @@
-import * as Popover from '@radix-ui/react-popover'
-import { IoCheckmarkSharp, IoFilter } from 'react-icons/io5'
+import {
+  PopoverTrigger,
+  Popover,
+  PopoverClose,
+  PopoverContent,
+} from '@/components/global/popover'
+import { IoCheckmarkSharp, IoFilterCircle } from 'react-icons/io5'
 import { LuX } from 'react-icons/lu'
 
 export function FilterByPriority() {
@@ -20,51 +25,46 @@ export function FilterByPriority() {
 
   return (
     <>
-      <Popover.Root>
-        <Popover.Trigger className="rounded-lg w-28 bg-transparent border px-1.5 py-2 text-zinc-700 gap-2 flex items-center justify-center">
-          <IoFilter />
-          <span>Status</span>
-        </Popover.Trigger>
+      <Popover>
+        <PopoverTrigger>
+          <IoFilterCircle size={24} />
+          <span>Prioridades</span>
+        </PopoverTrigger>
 
-        <Popover.PopoverPortal>
-          <Popover.Content
-            className="w-72 p-4 bg-white rounded-lg shadow-lg border"
-            sideOffset={5}
-          >
-            <div className="flex items-center justify-between w-full">
-              <h3 className="text-base font-medium ">Filtros</h3>
-              <Popover.Close>
-                <LuX />
-              </Popover.Close>
-            </div>
-            <div className="flex flex-col gap-2 mt-4">
-              <ul className="space-y-3">
-                {priorities.map((status, index) => (
-                  <li
-                    key={index}
-                    className="relative flex items-center justify-start gap-1.5"
+        <PopoverContent sideOffset={5} side="bottom" align="end">
+          <div className="flex items-center justify-between w-full">
+            <h3 className="text-base font-medium ">Filtros</h3>
+            <PopoverClose>
+              <LuX />
+            </PopoverClose>
+          </div>
+          <div className="flex flex-col gap-2 mt-4">
+            <ul className="space-y-3">
+              {priorities.map((status, index) => (
+                <li
+                  key={index}
+                  className="relative flex items-center justify-start gap-1.5"
+                >
+                  <input
+                    type="checkbox"
+                    className="peer relative h-5 w-5 shrink-0 appearance-none border border-gray-300 rounded-md duration-300  bg-white checked:bg-violet-500 hover:ring-1 hover:ring-violet-300 focus:outline-none"
+                    id={status.id}
+                  />
+                  <span className="absolute left-[2px] top-[2px] text-white opacity-0 peer-checked:opacity-100 pointer-events-none">
+                    <IoCheckmarkSharp size={16} />
+                  </span>
+                  <label
+                    htmlFor={status.id}
+                    className="text-sm text-gray-500 ms-3 dark:text-neutral-400"
                   >
-                    <input
-                      type="checkbox"
-                      className="peer relative h-5 w-5 shrink-0 appearance-none border border-gray-300 rounded-md duration-300  bg-white checked:bg-violet-500 hover:ring-1 hover:ring-violet-300 focus:outline-none"
-                      id={status.id}
-                    />
-                    <span className="absolute left-[2px] top-[2px] text-white opacity-0 peer-checked:opacity-100 pointer-events-none">
-                      <IoCheckmarkSharp size={16} />
-                    </span>
-                    <label
-                      htmlFor={status.id}
-                      className="text-sm text-gray-500 ms-3 dark:text-neutral-400"
-                    >
-                      {status.label}
-                    </label>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </Popover.Content>
-        </Popover.PopoverPortal>
-      </Popover.Root>
+                    {status.label}
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </PopoverContent>
+      </Popover>
     </>
   )
 }
