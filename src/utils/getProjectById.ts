11ea08e560f70437bcaeb185a2c727810283d20e
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-10-01 21:24:54 -0300

    FEAT: Cria função para buscar projeto pelo ID

---
+++ src/utils/getProjectById.ts
@@ -0,0 +1,29 @@
+import { FormDataProject } from '@/@types/schemas/FormSchemaProject'
+import { Session } from 'next-auth'
+
+export async function getProjectById(
+  id: string,
+  session: Session | null,
+): Promise<FormDataProject | null> {
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL
+
+  try {
+    const res = await fetch(`${baseUrl}/api/projects/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${session?.user.id ?? ''}`,
+      },
+    })
+
+    if (!res.ok) {
+      throw new Error('Erro ao buscar projeto')
+    }
+
+    const { project } = await res.json()
+    return project // Retorna o projeto com o cliente
+  } catch (error) {
+    console.error('Erro ao buscar projeto:', error)
+    return null // Retorna null em caso de erro
+  }
+}
